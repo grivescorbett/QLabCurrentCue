9b4e67eb3bfb9bda304660ec3a66c1ed353191cc
Gabe Rives-Corbett <gabe@gaberivescorbett.com>
2011-07-30 21:08:47 -0400

    initial import

---
+++ QLabCurrentCue/QLabCurrentCue.h
@@ -0,0 +1,13 @@
+//
+//  QLabCurrentCue.h
+//  QLabCurrentCue
+//
+//  Created by Gabe Rives-Corbett on 7/30/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+
+
+@interface QLabCurrentCue : NSObject
+
+@end
---
+++ QLabCurrentCue/QLabCurrentCue.m
@@ -0,0 +1,23 @@
+//
+//  QLabCurrentCue.m
+//  QLabCurrentCue
+//
+//  Created by Gabe Rives-Corbett on 7/30/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "QLabCurrentCue.h"
+
+@implementation QLabCurrentCue
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
